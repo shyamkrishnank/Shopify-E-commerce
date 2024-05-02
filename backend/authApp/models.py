aa4cf453a40e12b1ab4c6f504088b78b27dea57c
shyamkrishnank <shyamkrishnank18@gmail.com>
2024-05-02 17:36:50 +0530

    first commit

---
+++ backend/authApp/models.py
@@ -0,0 +1,6 @@
+import uuid
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
